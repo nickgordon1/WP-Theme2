de32138159e48ac0a53bf8a5aad307ced3e1c571
Nick Gordon <nicholasjgordon@hotmail.com>
2016-04-20 17:35:42 -0500

    Created a 2nd theme
    Just playing around with it.

---
+++ header.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>WP-Theme2</title>
+		<?php wp_head(); ?>
+	</head>
+	
+	<?php 
+		
+		if( is_front_page() ):
+			$wptheme2_classes = array( 'wptheme2-class', 'my-class' );
+		else:
+			$wptheme2_classes = array( 'no-wptheme2-class' );
+		endif;
+		
+	?>
+	
+	<body <?php body_class( $wptheme2_classes ); ?>>
+		
+		<div class="container">
+		
+			<div class="row">
+				
+				<div class="col-xs-12">
+					
+					<nav class="navbar navbar-default">
+					  <div class="container-fluid">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <a class="navbar-brand" href="#">WP-Theme2</a>
+					    </div>
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<?php 
+								wp_nav_menu(array(
+									'theme_location' => 'primary',
+									'container' => false,
+									'menu_class' => 'nav navbar-nav navbar-right'
+									)
+								);
+							?>
+						</div>
+					  </div><!-- /.container-fluid -->
+					</nav>
+				
+				</div>
+				
+			</div>
+			
+			<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
